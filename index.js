c91cbe48be2ca2af43aa7f960887c8c5d55c91bf
yorozc <yorozcolop49@gmail.com>
2024-08-12 22:41:01 -0700

    added some comments for planning

---
+++ index.js
@@ -0,0 +1,11 @@
+/*show a button that allows user to add a todolist
+once pressed, have the background fade out and a new page 
+pop out that allows the user to insert a title and
+a task
+Once reached the end of the container, have the cards flow
+downwards
+After this is completed, have a history page for deleted,
+and incomplete tasks
+- with the history, have a dropdown that shows all, incomplete, deleted
+Maybe create a priority tag for tasks as well
+*/
