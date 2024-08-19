2e6f6753677c00be0d8b57a6d455373c4498aa35
yorozc <yorozcolop49@gmail.com>
2024-08-19 13:11:56 -0700

    added edit button functionality and deleted clickoutside function

--- index.js
+++ index.js
@@ -12,16 +12,22 @@ Maybe create a priority tag for tasks as well
 
 //will hold the tasks 
 const tasks = [];
+let editingTaskId = null;
 
 const addTask = document.getElementById("task-button");
-const taskCardPopup = document.querySelector(".task-card-popup")
+const taskCardPopup = document.getElementById("task-card-popup")
 const taskContainer = document.getElementById("task-container");
 const taskCardInput = document.querySelector(".task-card-input");
 const saveTask = document.querySelector(".create-task");
+const titleInput = document.querySelector(".task-card-title");
+const contentInput = document.querySelector(".content-area");
+
+
 
 function showTasks(){
 
     taskContainer.innerHTML = '';
+
     tasks.forEach(task =>{
         const taskCard = document.createElement("div");
         taskCard.classList.add("task-card");
@@ -40,14 +46,16 @@ function showTasks(){
 
         const editBtn = document.createElement("button");
         editBtn.setAttribute("type", "button");
-        editBtn.id = "editBtn";
+        editBtn.classList.add("editBtn");
         editBtn.textContent = "Edit";
+        editButton(editBtn, taskCard);
 
         const deleteBtn = document.createElement("button");
         deleteBtn.setAttribute("type", "button");
-        deleteBtn.id = "deleteBtn"
+        deleteBtn.classList.add("deleteBtn");
         deleteBtn.textContent = "Delete";
-        deleteButton(deleteBtn, taskCard);
+        deleteButton(deleteBtn, taskCard); 
+        
 
         const saveBtn = document.createElement("button");
         saveBtn.setAttribute("type", "button");
@@ -63,6 +71,9 @@ function showTasks(){
         taskCard.appendChild(titleTaskCard);
         taskCard.appendChild(contentTaskCard);
         taskContainer.appendChild(taskCard);
+    
+
+        
     });
     
 
@@ -78,49 +89,69 @@ function createTask(title, content){
 
 addTask.addEventListener("click", ()=>{
     taskCardPopup.style.display = "flex";
+    titleInput.value = "";
+    contentInput.value = ""; 
+    editingTaskId = null;
 });
 
 
 
 saveTask.addEventListener("click", ()=>{
-    
+    const title = titleInput.value;
+    const content = contentInput.value;
     //values from input field
-    const title = document.querySelector(".task-card-title").value;
-    const content = document.querySelector(".content-area").value;
-
-    const newTask = createTask(title, content);
-    tasks.push(newTask);
+    if(editingTaskId){
+        const task = tasks.find(task => task.id == editingTaskId);
+        if(task){
+            task.title = title;
+            task.content = content;
+        }else{
+            console.log("Task is not found");
+        }
+        editingTaskId = null;
+    }
+    else{
+        const newTask = createTask(title, content);
+        tasks.push(newTask);
+    }
     showTasks();
-    document.querySelector(".task-card-title").value = "";
-    document.querySelector(".content-area").value = "";
-    taskCardPopup.style.display = "none";
+    taskCardPopup.removeAttribute("style");
     
 });
-
-function clickOutside(event){
-    if(!taskCardInput.contains(event.target) && !addTask.contains(event.target)){
-        taskCardPopup.style.display = "none";
-    }
-}
-
-document.addEventListener("click", clickOutside);
 
 //deletes taskcard from container and task from the list of tasks
 function deleteButton(deleteBtn, taskCard){
     deleteBtn.addEventListener("click", ()=>{
+        
         const taskIndex = tasks.findIndex(task => task.id = taskCard.id)
-        tasks.forEach(task =>{
-            if(task.id == taskCard.id){
+        tasks.forEach(task => {
+            if(task.id === taskCard.id){
                 taskCard.remove();
                 tasks.splice(taskIndex, 1);
+            }else{
+                console.log("Id not found!");
+            }
+        })
+    })
+}
+
+function editButton(editBtn, taskCard){
+    editBtn.addEventListener("click", ()=> {
+        tasks.forEach(task =>{
+            if(task.id == taskCard.id){
+                titleInput.value = task.title;
+                contentInput.value = task.content;
+                editingTaskId = taskCard.id
+                taskCardPopup.style.display = "flex";
+            }else{
+                console.log("Id not found!");
             }
         })
         
         
         
-    })
+    });
 }
-
 //when too much content is added to the task card, it doesn't overflow
 //have a transition when user presses add task
 //make an edit button for the task cards
